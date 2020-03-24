b99ae68abc2fa101ab8e58c4f512fa9169214d80
DerfetteJoel <derfettejoel@gmail.com>
2020-03-24 20:02:15 +0100

    Delete Main.py

--- Main.py
+++
@@ -1,8 +0,0 @@
-from api.Move import Move
-from api.Pokemon import Pokemon
-
-if __name__ == '__main__':
-    pokemon = Pokemon(1)
-    move = Move("flamethrower")
-    print(move.get_effectivity(pokemon))
-    print(pokemon.name)
